a1d58b34e0112358c020d53779d460e6f9ef274c
luisMunguia <luisma--munguia1@live.com.mx>
2023-08-29 13:04:50 -0600

    nueva columna status en tabla guias

--- database/migrations/2023_06_17_170940_create_guias_table.php
+++ database/migrations/2023_06_17_170940_create_guias_table.php
@@ -17,6 +17,7 @@ return new class extends Migration
             $table->string('descripcion');
             $table->string('urlvideo')->unique();
             $table->string('urlpdf')->unique();
+            $table->tinyInteger('status');
             $table->timestamps();
         });
 
